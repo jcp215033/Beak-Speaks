38a49f4ad455fe3988ca4c3f7f306d69e2a65cac
Julian Castro <jcas2tur@gmail.com>
2023-05-01 07:18:16 -0400

    jade endpoints

--- server/routes/random.js
+++ server/routes/random.js
@@ -3,19 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("random", { title: "Express" });
 });
 router.get("/1", function (req, res, next) {
-  res.render("index", { title: "1" });
+  res.render("random", { title: "1" });
 });
 router.get("/2", function (req, res, next) {
-  res.render("index", { title: "2" });
+  res.render("random", { title: "2" });
 });
 router.get("/3", function (req, res, next) {
-  res.render("index", { title: "3" });
+  res.render("random", { title: "3" });
 });
 router.get("/4", function (req, res, next) {
-  res.render("index", { title: "4" });
+  res.render("random", { title: "4" });
 });
 
 module.exports = router;
